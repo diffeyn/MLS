465d219ae5cc81c1f37841349ac92ca509d21ed9
Nathan D <diffeyn@gmail.com>
2025-08-01 16:19:05 -0500

    automation 4

--- src/scrape.py
+++ src/scrape.py
@@ -4,13 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 from selenium.webdriver.support.ui import Select
 from datetime import datetime
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 def set_up_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox') 
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    service = ChromeService(ChromeDriverManager().install()
+    driver = webdriver.Chrome(service = service, options=options)
     return driver
 
 
